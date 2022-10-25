834e5981450df652c6cf87533179205925686d50
sigmunht <sigmunht@uio.no>
2022-10-25 18:02:38 +0200

    hei

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiHkqeL3Pv6AhUbQ_EDHeYkClgQwqsBegQICRAF&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fasit til IN1000 eksamen!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
